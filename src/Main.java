24d784b186ef42fadbfbf9718cb38098366eed08
henigesa <121961371+henigesa@users.noreply.github.com>
2023-02-26 23:47:20 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // complete for nested loop for part 1
+        for (int i = 0; i < 6; i++) {
+            for (int c = 1; c <= i; c++ ) {
+                System.out.print("*");
+            }
+        System.out.println("");
+        }
+        //write for nested loop for part 2
+        for (int i = 0; i < 6; i++) {
+            for (int c = 5; c > i; c-- ) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        //write for nested loop for part 3
+        for (int i = 0; i < 5; i++) {
+            for (int c = 0; c < 6; c++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
